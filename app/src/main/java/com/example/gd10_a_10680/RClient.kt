0906171effdcc5780cc9d9450d34ce50c9966948
tebri-artanto <71914107+tebri-artanto@users.noreply.github.com>
2022-11-08 15:42:17 +0700

    Android Studio Files

---
+++ app/src/main/java/com/example/gd10_a_10680/RClient.kt
@@ -0,0 +1,16 @@
+package com.example.gd10_a_10680
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RClient {
+    private const val BASE_URL = "http://10.53.8.185/ci4-apiserver/public/"
+    val instances:api by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(api::class.java)
+    }
+
+}
---
+++ app/src/main/java/com/example/gd10_a_10680/ResponseDataMahasiswa.kt
@@ -0,0 +1,5 @@
+package com.example.gd10_a_10680
+
+import com.google.gson.annotations.SerializedName
+
+data class ResponseDataMahasiswa (@SerializedName("status") val stt:String, val totaldata: Int, val data:List<MahasiswaData>)
